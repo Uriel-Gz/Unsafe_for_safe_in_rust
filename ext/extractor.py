a17e7066b9b4e6a75970b822198b915d8670ac78
Uriel-Gz <uriguzman150212@gmail.com>
2025-01-08 20:34:00 -0300

    Implementación de la detección de bloques unsafe en archivos Rust y mejora en la lectura de archivos

--- ext/extractor.py
+++ ext/extractor.py
@@ -7,15 +7,18 @@ def indentify_unsafe_code_in_directory(directory):
     # Contador para bloques unsafe
     total_count = 0
 
+    file = open('unsafe_code.txt', 'w')
     # Recorre el directorio y sus subdirectorios
     for root, dirs, files in os.walk(directory):
         for filename in fnmatch.filter(files, '*.rs'):
             file_path = os.path.join(root, filename)
-            print(f'Procesando archivo: {file_path}')
-            
+
             # Lee el contenido del archivo
             with open(file_path, 'r', encoding='utf-8') as f:
                 code = f.read()
+            # Reemplaza la sección unsafe por otro código
+            # inside_unsafe = False
+            # count = 0
 
             pattern = r'unsafe\s*\{(?:[^{}]|\{(?:[^{}]|\{[^{}]*\})*\})*\}'
             matches = re.findall(pattern, code)
@@ -25,11 +28,9 @@ def indentify_unsafe_code_in_directory(directory):
             total_count += count
 
             for match in matches:
-                print(f'En archivo {file_path}: \n')
+                file.write(f'En archivo {file_path}: \n') 
+                file.write(f'{match} \n \n')
 
-            total_count += count
-
-    print(f'Cantidad total de bloques unsafe en los archivos .rs: {total_count}')
 
 # Llama a la función con la ruta del directorio
 indentify_unsafe_code_in_directory('../proyectos_para_analisar')
