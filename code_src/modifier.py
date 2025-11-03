6a5ff6758b97313b18639170721553c7ca1a1c2b
Uriel-Gz <uriguzman150212@gmail.com>
2025-11-03 10:40:49 -0300

    Modifico la escritura del archivo para respetar identacion

--- code_src/modifier.py
+++ code_src/modifier.py
@@ -3,6 +3,65 @@ import os
 from u_cases.u_analizer import UnsafeAnalyzer
 from lexer import Lexer
 
+
+def ub_delete(string):
+    lex = Lexer(string)
+    tokens = lex.tokenize()
+    new_code = []
+    pos = 0
+    while pos < len(tokens):
+        if tokens[pos].value == 'unsafe':
+            u_open = 1
+
+            #* Salta 'unsafe' y '{'
+            pos += 2
+            
+            while pos < len(tokens):
+                if tokens[pos].value == '{':
+                    u_open += 1
+                    new_code.append(tokens[pos])
+                elif tokens[pos].value == '}' and u_open-1 == 0:
+                    pos += 1
+                    u_open -= 1
+                    break
+                elif tokens[pos].value == '}' and u_open-1 > 0:
+                    u_open -= 1
+                    new_code.append(tokens[pos])
+                else:
+                    new_code.append(tokens[pos])
+                pos += 1
+        else:
+            new_code.append(tokens[pos])
+            pos += 1
+    
+    result_code = ''
+    space = ''
+
+    for item in new_code:
+        if item.type in ['COMMENT', 'RBRACE', 'LBRACE', 'SEMICOLON']:
+            if item.type == 'LBRACE':
+                space += '    '
+            elif item.type == 'RBRACE':
+                space = space[:-4]
+                result_code = result_code[:-4]
+            if item.type == 'SEMICOLON' or item.type == 'LBRACE':
+                result_code += ' ' + item.value + '\n'
+            else:
+                result_code += item.value + '\n'
+
+            result_code += space
+        elif item.type in ['LPAREN', 'RPAREN', 'COMMA', 'DOT', 'STRING_LITERAL']:
+            result_code += item.value
+        else:
+            if result_code and result_code[-1] not in [' ', '\n', '.', '(', '{', ';']:
+                result_code += ' '
+            result_code += item.value
+
+    return result_code
+    
+
+
+
 def replace_unsafe_code(origin):
     
     # Recorre el directorio y sus subdirectorios
@@ -21,21 +80,31 @@ def replace_unsafe_code(origin):
             if filename.endswith('.rs'):
                 
                 #* reconstruyo el codigo leido por lineas y lo tokenizo
-                code = '\n'.join(code)
+                code = ''.join(code)
                 lexer = Lexer(code)
                 tokens = lexer.tokenize()
 
                 pos = 0
                 while pos < len(tokens):
 
-                    if tokens[pos].type == 'UNSAFFE':
+                    if tokens[pos].value == 'unsafe':
+                        u_open = 1
+
                         #* Salta 'unsafe' y '{'
                         pos += 2
                         subcode = []
                         analizer = UnsafeAnalyzer()
+                        subcode.append(tokens[pos])
                         
-                        while pos < len(code):
-                            subcode.append(tokens[pos])
+                        while pos < len(tokens):
+                            if tokens[pos].value == '{':
+                                u_open += 1
+                            elif tokens[pos].value == '}' and u_open-1 == 0:
+                                u_open -= 1
+                                break
+                            elif tokens[pos].value == '}' and u_open-1 > 0:
+                                u_open -= 1
+
                             type_uc = analizer.analyze(subcode)
 
                             if type_uc == None:
@@ -45,11 +114,14 @@ def replace_unsafe_code(origin):
                             else:
                                 type_u = type_uc['type_u'] if type_uc != None else None
                                 methadata = type_uc['matched_data'] if type_uc != None else None
+                                
                                 code = type_u.replace(type_u, code, methadata)
-                                break
+                                new_code = code
+                                subcode = []
                     else:
-                        # new_code += subcode
+                        new_code = code
                         pos += 1
+                    new_code = ub_delete(new_code)
             else: 
                 new_code = '\n'.join(code)
 
